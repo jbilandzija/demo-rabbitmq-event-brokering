23d2cc2c8e38063eb43bffc599204c4b0245eea2
Josip Bilandzija <josip.bilandzija@novatec-gmbh.de>
2019-12-16 07:41:35 +0100

    init

---
+++ src/main/java/com/example/demo/EventPublisher.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class EventPublisher {
+
+    private static List<String> ROUTING_KEYS = Arrays.asList(
+            "customer.created",
+            "customer.edited",
+            "customer.deleted",
+            "order.created",
+            "order.edited",
+            "order.deleted",
+            "invoice.created",
+            "invoice.edited",
+            "invoice.deleted");
+    private final RabbitTemplate rabbitTemplate;
+
+    private final TopicExchange topicExchange;
+    private Logger logger = LoggerFactory.getLogger(EventPublisher.class);
+    private int messageNumber = 0;
+    private Random random = new Random();
+
+    @Autowired
+    public EventPublisher(RabbitTemplate rabbitTemplate, TopicExchange topicExchange) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.topicExchange = topicExchange;
+    }
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void sendMessage() {
+        String routingKey = randomRoutingKey();
+        String message = String.format("Event no. %d of type '%s'", ++messageNumber, routingKey);
+        rabbitTemplate.convertAndSend(topicExchange.getName(), routingKey, message);
+        logger.info("Published message '{}'", message);
+    }
+
+    private String randomRoutingKey() {
+        return ROUTING_KEYS.get(random.nextInt(ROUTING_KEYS.size()));
+    }
+
+}
